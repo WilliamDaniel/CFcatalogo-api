d490130b5218fcb0f06633403951b8d77a519dd5
William D. Oliveira <william.dev@outlook.com>
2021-11-16 15:47:45 -0300

    Added Genre.java

--- Genre.java
+++ Genre.java
@@ -1,10 +1,13 @@
 package domain.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Genre extends BaseEntity {
 
     private String name;
+    private List<Category> categories = new ArrayList<>();
 
     public Genre() {
     }
@@ -15,6 +18,17 @@ public class Genre extends BaseEntity {
 
     public Genre(UUID id, String name) {
         this.name = name;
+    }
+
+    public Genre(String name, List<Category> categories) {
+        this.name = name;
+        this.setCategories(categories);
+    }
+
+    public Genre(UUID id, String name, List<Category> categories) {
+        super.setId(id);
+        this.name = name;
+        this.setCategories(categories);
     }
 
     public String getName() {
@@ -31,4 +45,34 @@ public class Genre extends BaseEntity {
 
         this.name = name;
     }
+
+    public List<Category> getCategories() {
+        return this.categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        if (categories == null) {
+            throw new IllegalArgumentException("categories cannot be null");
+        }
+        if (categories.isEmpty()) {
+            throw new IllegalArgumentException("categories cannot be empty");
+        }
+
+        this.categories = categories;
+    }
+
+    public void addCategory(Category category) {
+        if (category == null) {
+            throw new IllegalArgumentException("category cannot be null");
+        }
+        this.categories.add(category);
+    }
+
+    public void removeCategory(Category category) {
+        if (category == null) {
+            throw new IllegalArgumentException("category cannot be null");
+        }
+        this.categories.removeIf(c -> this.categories.contains(category));
+    }
+
 }
